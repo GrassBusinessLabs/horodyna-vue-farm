720d49dfc8e6cd7a866bbd4340c5e451848e8886
Maks <Maks@DESKTOP-27U3HFL>
2024-01-03 12:27:19 +0200

    added missing files

---
+++ src/layouts/SettingsLayout.vue
@@ -0,0 +1,18 @@
+<template>
+   <v-layout class='rounded rounded-md'>
+      <app-header :header-title="'Налаштування'" />
+
+      <v-main class='bg-grey-lighten-4'>
+         <v-container class='pa-5 pt-6'>
+            <slot />
+         </v-container>
+      </v-main>
+   </v-layout>
+</template>
+
+<script lang='ts' setup>
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<style lang='scss' scoped>
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,10 @@
 <template>
    <home-layout>
-      <h1 class="centered-text">Мій профіль</h1>
+      <h1 class="centered-text">Підтримка</h1>
       <app-map-example />
    </home-layout>
 </template>
 <script lang='ts' setup>
-import AppMapExample from '@/components/AppMapExample.vue'
 import HomeLayout from '@/layouts/HomeLayout.vue'
 </script>
 <style>
